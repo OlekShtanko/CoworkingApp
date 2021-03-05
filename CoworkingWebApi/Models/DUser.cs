1ce8bb2c97dc80b935e7c4f4f7c83d01800e83c3
Лева Штанько <levalova2@gmail.com>
2021-03-05 19:21:53 +0300

    first commit

---
+++ CoworkingWebApi/Models/DUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoworkingWebApi.Models
+{
+    public class DUser
+    {
+        [Key]
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string fullName { get; set; }
+        public string mobile { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+    }
+}
